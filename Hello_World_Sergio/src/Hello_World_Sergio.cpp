9af07a27f87f0a27bc8d0a5afb2ed0edc5357a92
Sergio Garma <s.garma@alumnos.upm.es>
2017-11-30 02:15:40 +0100

    First commit

---
+++ Hello_World_Sergio/src/Hello_World_Sergio.cpp
@@ -0,0 +1,12 @@
+//============================================================================
+// Name        : Hello_World_Sergio.cpp
+// Author      : Sergio
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
